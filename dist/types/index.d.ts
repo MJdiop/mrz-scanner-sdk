27fed4ec59adc0d1136e0597b2a81cc58525b24f
Mbaye Jacques Diop <98955234+MJdiop@users.noreply.github.com>
2026-06-21 07:34:38 +0000

    Remove file from repository

--- dist/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { MrzScannerNative } from './native/MrzScannerNative';
-export { MrzScannerWeb } from './web/MrzScannerWeb';
-export { useSdkLicence } from './shared/licence';
-export { mapMlkitResult } from './shared/mrz-mapper';
-export { useScanner } from './shared/useScanner';
-export { sendImageToApi, sendUriToApi } from './shared/api-client';
-export type { MrzResult, MrzFields, DocumentType, ScanState, ApiConfig, ApiMode, SelfHostedConfig, CloudConfig, MrzScannerNativeProps, MrzScannerWebProps, MrzScannerBaseProps, SdkLicenceConfig, } from './shared/types';
-export type { LicenceState, LicenceStatus } from './shared/licence';
